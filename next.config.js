225a7bd65785c41d53abf9e396021cb9b6a9e228
abdurrozaq63 <rozaqmetalhead06@gmail.com>
2026-01-18 15:10:23 +0700

    fix isue

---
+++ next.config.js
@@ -0,0 +1,20 @@
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
